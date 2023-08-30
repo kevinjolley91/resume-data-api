3c86a033030f7b926ae10e519ff690e22c99331f
Kevin Jolley <jolley427@gmail.comm>
2023-08-30 13:27:23 -0400

    creates student model

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_30_172643) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "students", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone_number"
+    t.text "short_bio"
+    t.string "linkedin_url"
+    t.string "twitter_handle"
+    t.string "personal_blog_or_website_url"
+    t.string "online_resume_url"
+    t.string "github_url"
+    t.string "photo_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
