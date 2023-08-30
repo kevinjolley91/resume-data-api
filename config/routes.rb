d167adf5be956c08c0d43636d447d530da60d03a
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-08-30 13:43:26 -0400

    added an update method to experience

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get "/experiences" => "experiences#index"
   post "/experiences" => "experiences#create"
   get "/experiences/:id" => "experiences#show"
+  patch "/experiences/:id" => "experiences#update"
 end
