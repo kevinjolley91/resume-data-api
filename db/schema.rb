bd7034ca8c96b39fb6b0a1fed17ac667b6ce2f74
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-08-30 13:48:34 -0400

    created skills model and controller, added association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_30_172140) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_30_174653) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_30_172140) do
     t.string "job_title"
     t.string "company_name"
     t.text "details"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
