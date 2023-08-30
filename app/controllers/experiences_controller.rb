f244dd902d98f4dd2a661e672a643e09c8b1ac66
ADENA EISENBERG <ADENAEISENBERG@gmail.com>
2023-08-30 13:36:40 -0400

    added a create action

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -3,4 +3,16 @@ class ExperiencesController < ApplicationController
     @experiences = Experience.all
     render json: @experiences
   end
+
+  def create
+    @experience = Experience.create(
+      start_date: params[:start_date],
+      end_date: params[:end_date],
+      job_title: params[:job_title],
+      company_name: params[:company_name],
+      details: params[:details],
+    )
+    render json: @experience
+    # we won't be rendering json in the completed app
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get "/experiences" => "experiences#index"
+  post "/experiences" => "experiences#create"
 end
