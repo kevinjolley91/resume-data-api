c84532a2f2fae766cec335a87cc00ae17f274cd9
Kevin Jolley <jolley427@gmail.comm>
2023-08-30 13:49:59 -0400

    adds students controller and routes

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,60 @@
+class StudentsController < ApplicationController
+  def index
+    @students = Student.all
+    render json: @students
+  end
+
+  def show
+    @student = Student.find_by(id: params["id"])
+    render json: @student    
+  end
+
+  def create
+    @student = Student.create(
+      first_name = params[:first_name],
+      last_name = params[:last_name],
+      email = params[:email],
+      phone_number = params[:phone_number],
+      short_bio = params[:short_bio],
+      linkedin_url = params[:linkedin_url],
+      twitter_handle = params[:twitter_handle],
+      personal_blog_or_website_url = params[:personal_blog_or_website_url],
+      online_resume_url = params[:online_resume_url],
+      github_url = params[:github_url],
+      photo_url = params[:photo_url],
+    )
+    if @student.save
+      render json: @student
+    else
+      render json: { errors: @student.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  def update
+    @student = Student.find_by(id: params["id"])
+    @student.update(
+      first_name = params[:first_name] || @student.first_name,
+      last_name = params[:last_name] || @student.last_name,
+      email = params[:email] || @student.email,
+      phone_number = params[:phone_number] || @student.phone_number,
+      short_bio = params[:short_bio] || @student.short_bio,
+      linkedin_url = params[:linkedin_url] || @student.linkedin_url,
+      twitter_handle = params[:twitter_handle] || @student.twitter_handle,
+      personal_blog_or_website_url = params[:personal_blog_or_website_url] || @student.personal_blog_or_website_url,
+      online_resume_url = params[:online_resume_url] || @student.online_resume_url,
+      github_url = params[:github_url] || @student.github_url,
+      photo_url = params[:photo_url] || @student.photo_url,
+    )
+    if @student.save
+      render json: @student
+    else
+      render json: { errors: @student.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  def delete
+    @student = Student.find_by(id: params[""])
+    @student.destroy
+    render json: { message: "Student removed."}
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/students" => "students#index"
+  get "/students/:id" => "students#show"
+  post "/students" => "students#create"
+  patch "/students" => "students#update"
+  delete "/students/:id" => "students#delete"
 end
