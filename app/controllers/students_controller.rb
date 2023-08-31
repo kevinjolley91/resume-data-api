2becd55f4e3d1ec0edc4837d9b89a5147c11da9f
Kevin Jolley <jolley427@gmail.comm>
2023-08-31 12:44:37 -0400

    preparing for pull

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,10 @@
 class StudentsController < ApplicationController
   before_action :authenticate_student, except: [:index, :show, :create]
 
+  def student_allowed
+    user_signed_in? && (current_student.id == @student.id)
+  end
+
   def index
     @students = Student.all
     render json: @students
@@ -36,23 +40,28 @@ class StudentsController < ApplicationController
 
   def update
     @student = Student.find_by(id: params["id"])
-    @student.update(
-      first_name: params[:first_name] || @student.first_name,
-      last_name: params[:last_name] || @student.last_name,
-      email: params[:email] || @student.email,
-      phone_number: params[:phone_number] || @student.phone_number,
-      short_bio: params[:short_bio] || @student.short_bio,
-      linkedin_url: params[:linkedin_url] || @student.linkedin_url,
-      twitter_handle: params[:twitter_handle] || @student.twitter_handle,
-      "personal_blog/website_url": params[:"personal_blog/website_url"] || @student.personal_blog/website_url,
-      online_resume_url: params[:online_resume_url] || @student.online_resume_url,
-      github_url: params[:github_url] || @student.github_url,
-      photo: params[:photo] || @student.photo,
-    )
-    if @student.save
-      render json: @student
-    else
-      render json: { errors: @student.errors.full_messages }, status: :unprocessible_entity
+    # unless @student.id == current_student.id
+    #   flash[:notice] = "Access denied as your are not this student."
+    # end
+    if student_allowed
+      @student.update(
+        first_name: params[:first_name] || @student.first_name,
+        last_name: params[:last_name] || @student.last_name,
+        email: params[:email] || @student.email,
+        phone_number: params[:phone_number] || @student.phone_number,
+        short_bio: params[:short_bio] || @student.short_bio,
+        linkedin_url: params[:linkedin_url] || @student.linkedin_url,
+        twitter_handle: params[:twitter_handle] || @student.twitter_handle,
+        "personal_blog/website_url": params[:"personal_blog/website_url"] || @student.personal_blog/website_url,
+        online_resume_url: params[:online_resume_url] || @student.online_resume_url,
+        github_url: params[:github_url] || @student.github_url,
+        photo: params[:photo] || @student.photo,
+      )
+      if @student.save
+        render json: @student
+      else
+        render json: { errors: @student.errors.full_messages }, status: :unprocessible_entity
+      end
     end
   end
 
